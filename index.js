b9db0559c567ec5288302d92f59f1da037cf8820
a lan <bluenowte97@gmail.com>
2018-10-11 22:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // add solution here
+var musicianInstruments = [];
+
+ for(var i = 0; i < musicians.length; i++) {
+  var string = musicians[i] + " plays " +  instruments[i];
+  musiciansInstruments.push(string);
+  }
+
+ }
+
+return musicianInstruments;
+}
